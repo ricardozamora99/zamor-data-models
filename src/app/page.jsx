d24b7f9cc1162b68f22733f9bc67ed6433e9824e
ricardozamora99 <rjzamora1999@gmail.com>
2026-02-02 01:23:11 -0500

    Add GitHub Pages static export config

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+export default function RootRedirect() {
+  return (
+    <html>
+      <head>
+        <meta httpEquiv="refresh" content="0; url=./en/" />
+      </head>
+      <body />
+    </html>
+  );
+}
